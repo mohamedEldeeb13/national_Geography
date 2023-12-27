cdf3e760aac48151cf253d6d2e26e85c918b1141
mohamedEldeeb13 <mooel744@gmail.com>
2023-12-27 15:50:42 +0200

    Initial Commit

---
+++ national_Geography/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  national_Geography
+//
+//  Created by Mohamed Abd Elhakam on 27/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ national_Geography/national_GeographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  national_GeographyApp.swift
+//  national_Geography
+//
+//  Created by Mohamed Abd Elhakam on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct national_GeographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
